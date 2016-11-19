7e4f41813555440ad251695e263da56e45730396
iYefeng <yefeng38083120@126.com>
2016-11-19 12:14:31 +0800

    add files

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 SOURCE_DIR=`pwd`
 BUILD_DIR=${BUILD_DIR:-build}
 BUILD_TYPE=${BUILD_TYPE:-debug}
+BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
+INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install}
 
 if [ "$1"x = "all"x ]; then
     mkdir -p $BUILD_DIR/$BUILD_TYPE \
@@ -10,6 +12,7 @@ if [ "$1"x = "all"x ]; then
         && cmake \
         -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
         -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -DCMAKE_BUILD_NO_EXAMPLES=$BUILD_NO_EXAMPLES \
         $SOURCE_DIR \
         && make $*
 elif [ "$1"x = "clean"x ]; then
---
+++ src/tests/test1.cc
@@ -0,0 +1,21 @@
+/************************************************************
+ * Use of this source code is governed by a BSD-style license
+ * that can be found in the License file.
+ * 
+ * Author        : yefeng
+ * Email         : yefeng38083120@126.com
+ * Create time   : 2016-11-19 11:52
+ * Last modified : 2016-11-19 11:52
+ * Filename      : test1.cpp
+ * Description   : 
+ * *********************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  cout << "hello world!" << endl;
+  return 0;
+}
